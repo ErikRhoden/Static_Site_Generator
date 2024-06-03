d60971f83003f4b2091f8a01023a76b27bd4ce0d
zilharr <zilharrgalt@gmail.com>
2024-06-03 13:13:53 -0400

    Add ParentNode and testsuite

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,3 +33,21 @@ class LeafNode(HTMLNode):
         
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("The 'tag' parameter is required for ParentNode")
+        if not self.children:
+            raise ValueError("The 'children' parameter is required for ParentNode")
+        
+        children = ''
+        for child in self.children:
+            children += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
