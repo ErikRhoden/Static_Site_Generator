9984c08daec1efb4c00b80144efaaec206d00a22
zilharr <zilharrgalt@gmail.com>
2024-06-06 21:34:40 -0400

    Add Text_to_Textnodes and testsuite

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -121,6 +121,27 @@ class TestSplitImageLinks(unittest.TestCase):
         self.assertListEqual(new_no_links, [
                 TextNode("This is text without any links.", text_type_text)
             ])
+    
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes(
+            "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                TextNode("This is ", text_type_text),
+                TextNode("text", text_type_bold),
+                TextNode(" with an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word and a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" and an ", text_type_text),
+                TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and a ", text_type_text),
+                TextNode("link", text_type_link, "https://boot.dev"),
+            ],
+            nodes,
+        )
+
         
     
 
